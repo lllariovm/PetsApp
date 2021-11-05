78aeff30f8922cfdb4eac82932e1715f19224068
Mario Valencia <vmmario@gmail.com>
2021-11-05 12:09:40 -0500

    Initial Commit

---
+++ PetsApp/PetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsAppApp.swift
+//  PetsApp
+//
+//  Created by Mario Valencia Morales on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct PetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
